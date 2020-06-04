6df88ce638daa263fcb7e96421534877192b41c5
MatiasRizzatoBusta <matias.rizzato@gmail.com>
2020-06-04 09:27:12 -0300

    corrigo pto 1

--- src/Lib.hs
+++ src/Lib.hs
@@ -26,12 +26,12 @@ data Tiro = UnTiro {
 type Puntos = Int
 
 ---------------------------- Punto 1 ----------------------------
-type Palos = Int->Jugador->Tiro
+type Palos = Jugador->Tiro
 putter :: Palos--los hago asi pq para hacer la lista de funciones todos tienen que tener la misma forma
-putter _ jugador = UnTiro{velocidad=10,precision=calculoPrecision (*2) (habilidad jugador),altura= 0}
+putter jugador = UnTiro{velocidad=10,precision=calculoPrecision (*2) (habilidad jugador),altura= 0}
 madera :: Palos
-madera _ jugador = UnTiro{velocidad=100,precision=calculoPrecision (`div` 2) (habilidad jugador),altura= 5}
-hierros :: Palos--revisar como seria lo del N
+madera jugador = UnTiro{velocidad=100,precision=calculoPrecision (`div` 2) (habilidad jugador),altura= 5}
+hierros :: Int->Palos --hago una funcion generica que modele a todos los hierros
 hierros n jugador = UnTiro{velocidad=calculoVelocidad n (habilidad jugador),precision=calculoPrecision (`div` n) (habilidad jugador),altura= max 0 (n-3)}
 
 calculoVelocidad :: Int->Habilidad->Int
@@ -40,7 +40,9 @@ calculoVelocidad n  = (*n).fuerzaJugador
 calculoPrecision :: (Int->Int)->Habilidad->Int
 calculoPrecision funcion = funcion.precisionJugador
 
-palos = [putter,madera,hierros]
+palos ::[Palos]
+palos = [putter,madera] ++ map hierros [1..10]--esto me da una lista con todos los hierros y un valor
+
 ---------------------------- Punto 2 ----------------------------
 golpe :: Jugador->Int->Palos->Tiro
 golpe jugador n palo = palo n jugador 
